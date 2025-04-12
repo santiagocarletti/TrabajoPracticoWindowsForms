75acb12ac3bede1ce17d551129a7f9edc032fc09
Santiago Carletti <carlettisantiago@gmail.com>
2025-04-12 15:57:25 +0200

    Comienzo desarrollo clase Articulo

---
+++ TpWindowsForms/WinFormsApp/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+       }
+}
