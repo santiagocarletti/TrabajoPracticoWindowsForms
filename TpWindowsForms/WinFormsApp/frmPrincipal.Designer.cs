90e9bb63e2d7b7c4bdf68b44e4010e38febf3c86
fraanfolino <fran_fol@hotmail.com>
2025-04-12 14:26:14 -0300

    Creacion de clase Marca e inicio de formulario principal

---
+++ TpWindowsForms/WinFormsApp/frmPrincipal.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WinFormsApp
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnListado = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDetalle = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(185, 24);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(388, 33);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "GESTION DE ARTICULOS";
+            // 
+            // btnListado
+            // 
+            this.btnListado.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnListado.FlatAppearance.BorderSize = 3;
+            this.btnListado.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnListado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListado.Location = new System.Drawing.Point(149, 119);
+            this.btnListado.Name = "btnListado";
+            this.btnListado.Size = new System.Drawing.Size(144, 43);
+            this.btnListado.TabIndex = 1;
+            this.btnListado.Text = "LISTAR";
+            this.btnListado.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAgregar.FlatAppearance.BorderSize = 3;
+            this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Location = new System.Drawing.Point(149, 222);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(144, 45);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "AGREGAR ";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnEliminar.FlatAppearance.BorderSize = 3;
+            this.btnEliminar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Location = new System.Drawing.Point(149, 334);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(144, 52);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalle
+            // 
+            this.btnDetalle.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnDetalle.FlatAppearance.BorderSize = 3;
+            this.btnDetalle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnDetalle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetalle.Location = new System.Drawing.Point(481, 334);
+            this.btnDetalle.Name = "btnDetalle";
+            this.btnDetalle.Size = new System.Drawing.Size(150, 52);
+            this.btnDetalle.TabIndex = 4;
+            this.btnDetalle.Text = "DETALLE";
+            this.btnDetalle.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnBuscar.FlatAppearance.BorderSize = 3;
+            this.btnBuscar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Location = new System.Drawing.Point(478, 119);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(153, 43);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnModificar.FlatAppearance.BorderSize = 3;
+            this.btnModificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Location = new System.Drawing.Point(478, 222);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(153, 45);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "MODIFICAR ";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnDetalle);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnListado);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnListado;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDetalle;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+    }
+}
