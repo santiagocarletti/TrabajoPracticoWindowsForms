6491d998ac0fe357a55ebcbfbaaaeb73cb999705
Nicolas Calcagno <calcagno.nico@gmail.com>
2025-04-13 15:45:02 -0300

    Agregado AccesoBD, CategoriaNegocio, MarcaNegocio

---
+++ TpWindowsForms/negocio/CategoriaNegocio.cs
@@ -0,0 +1,43 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    internal class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoBD datos = new AccesoBD();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion from CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lectorbd.Read())
+                {
+                    Categoria aux = new Categoria();
+
+                    aux.Id = (int)datos.Lectorbd["Id"];
+                    aux.Descripcion = (string)datos.Lectorbd["Descripcion"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ TpWindowsForms/negocio/MarcaNegocio.cs
@@ -0,0 +1,42 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    internal class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoBD datos = new AccesoBD();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion From MARCAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lectorbd.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lectorbd["Id"];
+                    aux.Descripcion = (string)datos.Lectorbd["Descripcion"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
